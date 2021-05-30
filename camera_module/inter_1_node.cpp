888f91159c8f80d4c1127cbcfcb086c83addc7d1
rollingkit <85065206+rollingkit@users.noreply.github.com>
2021-05-30 14:23:03 -0400

    First draft of intermediate node
    Read in x coordinate value from 'detections' topic to which the 'detectnet' node publishes.

---
+++ camera_module/inter_1_node.cpp
@@ -0,0 +1,17 @@
+#include <ros/ros.h>
+#include <vision_msgs/Detection2DArray.h>
+#include <std_msgs/String.h>
+
+void callback_receive_detect(const vision_msgs::Detection2DArray::SharedPtr det_msg){
+  ROS_INFO("%f", det_msg->detections[0].bbox.center.x);
+}
+
+int main(int argc, char **argv){
+  ros::init(argc, argv, "inter_2");
+  ros::NodeHandle nh;
+
+  ros::Subscriber sub = nh.subscribe("detectnet/detections", 1000, callback_receive_detect);
+
+  ros::spin();
+
+}
