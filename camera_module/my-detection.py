0e98c173b7bf6c6e30907af71e0376bd74b0ec80
rollingkit <85065206+rollingkit@users.noreply.github.com>
2021-05-30 14:39:47 -0400

    Add files via upload

---
+++ camera_module/my-detection.py
@@ -0,0 +1,14 @@
+import jetson.inference
+import jetson.utils
+
+#load the object detection model
+net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+camera = jetson.utils.videoSource("csi://0")
+display = jetson.utils.videoOutput("display://0") # ''my_video.mp4' for file
+
+while display.IsStreaming():
+	img = camera.Capture()
+	detections = net.Detect(img)
+	display.Render(img)
+	display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+
