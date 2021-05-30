a3d590980c5b3c17926cf21e8aa0d8f4fc61977d
rollingkit <85065206+rollingkit@users.noreply.github.com>
2021-05-30 15:26:12 -0400

    Added publisher and global variables
    String publisher: to print stop command to arm node when the detected object is centred. 
    Variables: isdetect to check when the camera is able to actually detect any object. 
    upper and lower limit global variables for keeping track of when detected object is at the centre of the view.

--- ros_interaction/inter_2_node.cpp
+++ ros_interaction/inter_2_node.cpp
@@ -5,8 +5,18 @@
 //global variables
 float x_co;
 float y_co:
+bool isdetect;
+
+float u_xlimit;
+float l_xlimit;
+float u_ylimit;
+float l_ylimit;
 
 void callback_receive_detect(const vision_msgs::Detection2DArrayConstPtr det_msg){
+  if(det_msg->detections[0]){
+    isdetect = true;
+  }
+  ROS_INFO(isdetect ? "true" : "false");
   x_co = det_msg->detections[0].bbox.center.x;
   y_co = det_msg->detections[0].bbox.center.y;
   ROS_INFO("%f", det_msg->detections[0].bbox.center.x);
@@ -16,8 +26,21 @@ void callback_receive_detect(const vision_msgs::Detection2DArrayConstPtr det_msg
 int main(int argc, char **argv){
   ros::init(argc, argv, "inter_2");
   ros::NodeHandle nh;
-
+  
+  //subscriber to monitor x and y coordinates of detection
   ros::Subscriber sub = nh.subscribe("detectnet/detections", 1000, callback_receive_detect);
+  
+  //publisher to publish stop command to robotic arm node
+  ros::Publisher pub = nh.advertise<std_msgs::String>("/robot_status", 10);
+  
+  while(ros::ok()){
+    if((x_co > l_xlimit) && (x_co < u_xlimit)){
+      std_msgs::String msg;
+      msg.data = "Stop Wrist Rotate";
+      pub.publish(msg);
+    }
+    
+  }
 
   ros::spin();
 
